98f78da47d518313b5e0dcbcc5cebaf7e60c7b9e
samusilva310109-a11y <samusilva310109@gmail.com>
2026-04-29 20:31:42 -0300

    Realizado a estilização da categoria e reformulada as configurações de responsividade. Adicionado descrição aos produtos.

--- app.js
+++ app.js
@@ -13,29 +13,39 @@ function criarCard(produtos) {
     const nomeProduto = document.createElement('h3')
     nomeProduto.textContent = produtos.nome
 
+    const descricaoProduto = document.createElement('p')
+    descricaoProduto.textContent = produtos.descricao
+    descricaoProduto.className = "descricao"
+
     const categoraiProduto = document.createElement('span')
     categoraiProduto.textContent = produtos.categoria
+    let categoriaFormat = formatarCategoria(produtos.categoria)    
+    categoraiProduto.classList.add(categoriaFormat, 'categoria')
     
     
     const preco = document.createElement('span')
     preco.textContent = formatarNumero(produtos.preco)
+    preco.className = 'preco'
 
-    card.append(imagem, nomeProduto, categoraiProduto, preco)
+    card.append(imagem, nomeProduto,descricaoProduto, categoraiProduto, preco)
 
     return card
 }
 
 function formatarNumero(numero){
-    
-
     const formatter = new Intl.NumberFormat('pt-BR', {
     style: 'currency',
     currency: 'BRL',
     }).format(numero)
-
     
-
     return formatter
+}
+
+function formatarCategoria(categoria){
+    if (String(categoria).toLowerCase() == 'Eletrônicos'.toLowerCase()) {
+        return 'corEletronicos'
+    }else if(String(categoria).toLowerCase() == 'Informática'.toLowerCase())
+        return 'corInformatica'
 }
 
 const cards = produtos.map(criarCard)
